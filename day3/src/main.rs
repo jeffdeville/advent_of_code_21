5e639171ac7241101b06ea752021c71686b0b483
Jeff Deville <jeffdeville@gmail.com>
2021-12-29 16:31:31 -0700

    Day 3 Part 2 working, but HIDEOUS

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -7,6 +7,9 @@ fn main() {
     println!("Power Level: {}", get_power_level(&readings));
     let o2_gen_rating = get_o2_generator_ratings(&readings);
     println!("O2 Gen Rating: {}", o2_gen_rating);
+    let co2_gen_rating = get_co2_generator_ratings(&readings);
+    println!("CO2 Gen Rating: {}", co2_gen_rating);
+    println!("Life Support Rating: {}", o2_gen_rating * co2_gen_rating);
 }
 
 fn convert_to_decimal(input: &Vec<u8>) -> u32 {
@@ -15,11 +18,11 @@ fn convert_to_decimal(input: &Vec<u8>) -> u32 {
     result
 }
 
-fn sum_col_at_index(readings: &Vec<String>, col: usize) -> u8 {
-    let (vals, num_lines) = sum_cols_vertically(readings);
+// fn sum_col_at_index(readings: &Vec<String>, col: usize) -> u8 {
+//     let (vals, num_lines) = sum_cols_vertically(readings);
 
-    if vals[col] as f32 / num_lines as f32 >= 0.5 { 1 } else { 0 }
-}
+//     if vals[col] as f32 / num_lines as f32 >= 0.5 { 1 } else { 0 }
+// }
 
 fn sum_cols_vertically(readings: &Vec<String>) -> (Vec<i32>, u32) {
     // Make a Vec<i32> and then sum the digits in the whole list, and then if
@@ -58,7 +61,7 @@ fn get_power_level(readings: &Vec<String>) -> u32 {
     gamma_dec * epsilon
 }
 
-fn get_o2_generator_ratings(readings: &Vec<String>) -> i32 {
+fn get_o2_generator_ratings(readings: &Vec<String>) -> u32 {
     // Keep track of current bit to examine
     // go through, flat map a length 1 slick at that bit
     // sum
@@ -94,9 +97,47 @@ fn get_o2_generator_ratings(readings: &Vec<String>) -> i32 {
 
         curr_bit += 1;
     }
-    println!("{:?}", filtered_list);
-    23
-    // convert_to_decimal(filtered_list.first())
+    convert_to_decimal(&filtered_list[0])
+}
+
+fn get_co2_generator_ratings(readings: &Vec<String>) -> u32 {
+    // Keep track of current bit to examine
+    // go through, flat map a length 1 slick at that bit
+    // sum
+    // divide by len of that vec
+    // figure 0 or 1
+    // filter list
+    // if length == 1,
+    //      convert to decimal
+    // if length > 1
+    //      next bit
+    //      loop
+
+    let mut curr_bit = 0;
+    let mut filtered_list  = readings
+        .iter()
+        .map(|line| line.chars().map(|ch| if ch == '1' { 1 } else { 0 }).collect())
+        .collect::<Vec<Vec<u8>>>();
+
+    while filtered_list.len() > 1 {
+        let sum_vals_in_col = filtered_list
+            .iter()
+            .fold(0, |acc, vec| acc + (*vec.get(curr_bit).unwrap() as u32));
+        let target_bit: u8 = if sum_vals_in_col as f32 / filtered_list.len() as f32 >= 0.5 { 0 } else { 1 };
+        // I'm stuck here because I can't repeatedly filter into the same variable,
+        // because something about "filter" returns references to entries, rather than the entries themselves
+        filtered_list = filtered_list
+            .iter()
+            .filter(|vec| {
+                (*vec).get(curr_bit).unwrap() == &target_bit
+            })
+            .map(|line| line.clone())
+            .collect::<Vec<Vec<u8>>>();
+
+        curr_bit += 1;
+    }
+    println!("{:?}", filtered_list[0]);
+    convert_to_decimal(&filtered_list[0])
 }
 
 #[test]
@@ -114,4 +155,30 @@ fn test_get_power_level() {
     ];
     // Should result in 0011 == 3 and 111111111100 == 4092, multiplied to get 12276
     assert_eq!(get_power_level(&input), 12276);
+}
+
+#[test]
+fn test_get_o2_rating() {
+    let input = vec![
+        "011".to_string(),
+        "010".to_string(),
+        "111".to_string()
+    ];
+    // most common bit: 0
+    // most common bit: 1
+    // most common bit: 1
+    assert_eq!(get_o2_generator_ratings(&input), 3);
+}
+
+#[test]
+fn test_get_co2_rating() {
+    let input = vec![
+        "011".to_string(),
+        "010".to_string(),
+        "111".to_string()
+    ];
+    // least common bit: 1
+    // least common bit: 1
+    // least common bit: 1
+    assert_eq!(get_co2_generator_ratings(&input), 7);
 }
